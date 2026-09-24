49750beaf3d45ecb0b2df1703adb9b2cb3083472
Christopher Klint <christopher.klint@gmail.com>
2026-09-24 11:50:15 +0200

    Improve touch target sizes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'small-start-v6';
+const CACHE = 'small-start-v7';
 const SHELL = ['./', './index.html', './styles.css', './app.js', './timer.js', './manifest.webmanifest', './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png', './fonts/dm-sans-400.ttf', './fonts/dm-sans-700.ttf', './fonts/manrope-700.ttf', './fonts/manrope-800.ttf'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL.map(path => new URL(path, self.registration.scope)))).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(Promise.all([caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))), self.clients.claim()])); });
